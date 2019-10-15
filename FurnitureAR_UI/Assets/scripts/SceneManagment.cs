47f8e24220f26586c34b9c8500dd9abaee6a36e1
Rabbia Rauf <55631855+RabbiaRauf@users.noreply.github.com>
2019-10-15 21:55:38 +0500

    Add files via upload

---
+++ FurnitureAR_UI/Assets/scripts/SceneManagment.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneManagment : MonoBehaviour
+{
+    public void GotoLandingScene()
+    {
+        SceneManager.LoadScene("landingscreen");
+    }
+
+    public void GotoAboutScene()
+    {
+        SceneManager.LoadScene("aboutus");
+    }
+
+    public void GotoCategoryScene()
+    {
+        SceneManager.LoadScene("category");
+    }
+
+    public void GotoPrtoductDetailScene()
+    {
+        SceneManager.LoadScene("productdetail");
+    }
+
+    public void GotoSigninSignupScene()
+    {
+        SceneManager.LoadScene("signin_signup");
+    }
+
+    public void GotoSigninScene()
+    {
+        SceneManager.LoadScene("signin");
+    }
+
+    public void GotoSignupScene()
+    {
+        SceneManager.LoadScene("signup");
+    }
+
+    public void GotoUserProfileScene()
+    {
+        SceneManager.LoadScene("userprofile");
+    }
+}
