a61a6e15cdcc51e74c17cab5c9f212c376a37990
shariqkhanyousufzai <shariqkhanyousufzai@outlook.com>
2019-10-20 20:53:06 +0500

    Color Change

---
+++ color_changing/Assets/Scripts/MaterialChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChange : MonoBehaviour
+{
+    public Material matRed;
+    public Material matGreen;
+    public GameObject Sphere;
+
+    // Start is called before the first frame update
+    // GetComponent<Renderer>().material = matRed;
+    public void MatRed()
+    {
+        GameObject sphereBall = GameObject.Find("Sphere");
+        sphereBall.GetComponent<Renderer>().material = matRed;
+
+    }
+
+    public void MatGreen()
+    {
+        GameObject sphereBall = GameObject.Find("Sphere");
+        sphereBall.GetComponent<Renderer>().material = matGreen;
+    }
+    
+}
